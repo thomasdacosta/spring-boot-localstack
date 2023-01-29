ae7ffb745b044c76915bc7640c07c4ee35e07a66
Thomás da Costa <thomasdacosta@gmail.com>
2023-01-29 17:52:02 -0300

    first commit

---
+++ localstack/localstack.bat
@@ -0,0 +1,2 @@
+@echo off
+aws --endpoint http://localhost:4566 --profile localstack ssm put-parameter --name "/config/spring-boot-localstack_localstack/helloWorld" --value "Hello World Parameter Store" --type String
---
+++ src/test/java/br/com/thomasdacosta/SpringBootLocalstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thomasdacosta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLocalstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
