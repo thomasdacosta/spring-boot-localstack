c625a9753748ef20ecf996d07a60f7f4bfd0ac66
Thomas da Costa <thomasdacosta@gmail.com>
2023-03-19 16:36:55 -0300

    fix: ajustes no projeto

--- src/test/java/br/com/thomasdacosta/SpringBootLocalstackApplicationTests.java
+++ src/test/java/br/com/thomasdacosta/SpringBootLocalstackApplicationTests.java
@@ -3,7 +3,6 @@ package br.com.thomasdacosta;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SpringBootLocalstackApplicationTests {
 
 	@Test
